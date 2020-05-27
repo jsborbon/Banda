7063eb7e93395c99794ccb387624a049eb0fc1dc
Javier Borbon <49053441+jsborbon@users.noreply.github.com>
2020-05-26 23:27:00 -0500

    Mariachis con explicación

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask, render_template, session
 from logic.Band import Band
 
@@ -31,3 +32,38 @@ def play():
 
 if __name__ == "__main__":
     app.run(debug=True)
+=======
+from flask import Flask, render_template, session
+from logic.Band import Band
+
+
+app = Flask(__name__)
+app.secret_key = 'This-is-a-real-secret-key'
+b = Band()
+tuneBand = b.tuneBand()
+playBand = b.playBand()
+
+
+@app.route('/Mariachi')
+def index():
+    assignedBandL = session['Band']
+    tuneBandL = tuneBand
+    return render_template('Mariachi.htm', assignedBand=assignedBandL, tuneBand=tuneBandL)
+
+
+@app.route('/')
+def prueba():
+    session['Band'] = b.assignBand()
+    return render_template('Index.htm')
+
+
+@app.route('/Play')
+def play():
+    assignedBandL = session['Band']
+    playBandL = playBand
+    return render_template('playBand.htm', assignedBand=assignedBandL, playBand=playBandL)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+>>>>>>> 1dce1361caed8500093f8aac1a98a176b319de1b
